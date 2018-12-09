e95519f0da9c905f5ffc0efa42c962aabadc3717
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-09 10:03:18 -0800

    cli is no longer an exception for being able to run basic make commands

--- update.sh
+++ update.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-pushd cli
-	npm update
-	VERSION=$(npm version patch)
-	git add .
-	git commit -m "${VERSION}: ${MSG}"
-	git push
-	npm publish --access public
-popd
-
-declare -a REPOS=("utilities" "performer" "compiler" "pattern" "playroom" "lab")
+declare -a REPOS=("cli" "utilities" "performer" "compiler" "pattern" "playroom" "lab")
 for REPO in "${REPOS[@]}"
 do
 	pushd ${REPO}
