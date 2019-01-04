37296daa50961db217d67fc3bd06b49b46ceef56
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 10:17:10 -0800

    ability to ship starting from a repo you left off at

--- ship.sh
+++ ship.sh
@@ -2,15 +2,24 @@
 
 set -e
 
-declare -a REPOS=("cli" "utilities" "performer" "compiler" "pattern" "registry" "playroom" "lab")
-for REPO in "${REPOS[@]}"
-do
-	pushd ${REPO}
-		npm update
-		make ship MSG="${MSG}"
-	popd
+REPOS=("cli" "utilities" "performer" "compiler" "pattern" "registry" "playroom" "lab")
+FROM_INDEX=0
+for i in "${!REPOS[@]}" ; do
+	if [[ "${REPOS[i]}" = "${FROM}" ]] ; then
+		FROM_INDEX=i
+		break
+	fi
 done
 
-git add .
-git commit -m "${MSG}"
-git push
+REPOS=("${REPOS[@]:FROM_INDEX}")
+
+for REPO in "${REPOS[@]}"
+do
+	pushd ${REPO} > /dev/null 2>&1
+		npm update
+		make ship MSG="${MSG}"
+	popd > /dev/null 2>&1
+done
+
+ make commit
+ make push
