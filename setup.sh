b4dd9bf55ccd4ad9a356ecd304fa22d4c2a536cf
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-05 09:03:24 -0800

    dont have cli when youre setting up

--- setup.sh
+++ setup.sh
@@ -32,4 +32,10 @@ fi
 git config --global core.autocrlf false
 git config --global core.eol lf
 
-make pull
+pull_recursively() {
+	git checkout master || return
+	git submodule update --init --recursive || return
+	git pull -r || return
+	git submodule foreach pull_recursively
+}
+pull_recursively
