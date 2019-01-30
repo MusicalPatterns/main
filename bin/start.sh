ce56d87fd83a8582673b59b1d2fd914fbf5c0c16
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-30 12:11:07 -0800

    color wds

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pushd services/lab > /dev/null 2>&1
-	node -r ts-node/register --max-old-space-size=4096 node_modules/webpack-dev-server/bin/webpack-dev-server.js --config webpack.browser.js
+	node -r ts-node/register --max-old-space-size=4096 node_modules/webpack-dev-server/bin/webpack-dev-server.js --config webpack.browser.js --color
 popd > /dev/null 2
