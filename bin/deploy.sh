9e7b341b897af66cd3af742a7ad1015df30d077f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-15 12:51:12 -0700

    lab in prod was not in debug mode

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 gcloud config configurations activate musical-patterns
 pushd services/lab > /dev/null 2>&1
-	node -r ts-node/register --max-old-space-size=4096 node_modules/webpack/bin/webpack.js --config webpack.deploy.js
+	NODE_ENV=production node -r ts-node/register --max-old-space-size=4096 node_modules/webpack/bin/webpack.js --config webpack.deploy.js
 	gcloud app deploy -q
 popd > /dev/null 2>&1
